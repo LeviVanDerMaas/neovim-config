a9634000053df289a36d20f02b63be133a75b17c
Levi van der Maas <levi.vdmaas@gmail.com>
2026-06-13 15:58:03 +0200

    lazydev: will now also be auto enabled for files named ".nvim.lua"

--- lua/plugins/ftmodules/lazydev.lua
+++ lua/plugins/ftmodules/lazydev.lua
@@ -2,6 +2,10 @@
 require("lazydev").setup {
   library = { path = "${3rd}/luv/library", words = { "vim%.uv" } },
   enabled = function(root_dir)
+    vim.notify(vim.fn.expand("%:t"))
     return vim.g.lazydev_enabled
+      -- Server may need to be restarted if it already exists prior to entering .nvim.lua,
+      -- then again restarted after deleting that buffer to clear environment
+      or (vim.fn.expand("%:t") == ".nvim.lua" and vim.g.lazydev_enabled ~= false)
   end
 }
