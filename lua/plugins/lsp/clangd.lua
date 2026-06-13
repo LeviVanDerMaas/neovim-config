efb31876c0cdf82edcee03e552cc3bf290ce77d9
Levi van der Maas <levi.vdmaas@gmail.com>
2026-06-13 15:56:38 +0200

    clangd: now dim inactive preproccesor branches instead of greying out

---
+++ lua/plugins/lsp/clangd.lua
@@ -0,0 +1,12 @@
+vim.lsp.config("clangd", {
+  on_attach = function ()
+    -- "Dim out" inactive preprocessor branches while preserving their syntax
+    -- highlighting. Default settings would grey them out like comments instead.
+    -- This is because, for Neovim, Clangd uses the "comment" LSP
+    -- token to signal such inactive branches (and does not use it otherwise).
+    -- https://clangd.llvm.org/features#kinds
+    vim.api.nvim_set_hl(0, "@lsp.type.comment.cpp", {
+      dim = true
+    })
+  end
+})
